82227322cfe2aff0c089aec0bda9a72fbb4fff91
Jason Duquain <jduquain@gmail.com>
2019-08-11 15:18:54 -0400

    updated pie chart

---
+++ InfoGraphics/pie-charts-w-cssvars/code at thats not right caption/script.js
@@ -0,0 +1,27 @@
+
+
+const pie = document.querySelector('.pie');
+const actions = document.querySelector('.actions');
+const segments = pie.children;
+
+
+
+/* COMPLETE
+
+const pie = document.querySelector('.pie');
+const actions = document.querySelector('.actions');
+const segments = pie.children;
+
+const updateSegment = e => {
+  const idx = [...actions.children].indexOf(e.target);
+  const key = idx % 2 === 0 ? 'offset' : 'value';
+  const toUpdate = segments[Math.floor(idx / 2) - 1];
+  toUpdate.style.setProperty(`--${key}`, e.target.value);
+};
+
+window.addEventListener('input', updateSegment);
+
+
+
+*/
+
