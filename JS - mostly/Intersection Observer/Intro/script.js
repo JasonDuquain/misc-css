ae83ce3bc92c9497afdcfbd0181ffaf4d95e942e
Jason Duquain <jduquain@gmail.com>
2019-09-02 11:33:01 -0400

    intro observer

---
+++ JS - mostly/Intersection Observer/Intro/script.js
@@ -0,0 +1,25 @@
+
+const sectionOne = document.querySelector('.section1');
+const sections = document.querySelectorAll('section');
+
+const options = {
+    root: null,
+    threshold: 0,
+    rootMargin: "-150px"
+}
+
+const observer = new IntersectionObserver(function(entries, observer) {
+    entries.forEach(entry => {
+        //console.log(entry);
+        entry.target.classList.toggle('inverse');
+    });
+}, options);
+
+sections.forEach(section => {
+    observer.observe(section);
+})
+
+observer.observe(sectionOne);
+
+
+
